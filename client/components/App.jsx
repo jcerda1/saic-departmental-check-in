a7fdc1ba0a4941c491dcf710412fef1c4e5d8b28
Nick Havens <npahavens@gmail.com>
2017-12-07 20:21:28 -0600

    starter files

---
+++ client/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import axios from 'axios';
+import { BrowserRouter, Route, Link, browserHistory } from 'react-router-dom';
+import '../css/styles.css';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  render() {
+
+  }
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,67 @@
+if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test') {
+  require('dotenv').config();
+}
+
+const express = require('express')
+const bodyParser = require('body-parser');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const app = express();
+
+
+const path = require('path');
+const port = process.env.PORT || 3000;
+const isAuthenticated = require('./controllers/authroutes.js').isAuthenticated;
+
+let config;
+(port === 3000)? config = require('../webpack.dev.js') : config = require('../webpack.prod.js');
+const compiler = webpack(config);
+
+
+
+app.use(express.static(__dirname));
+
+app.use(passport.initialize());
+app.use(expressValidator())
+
+const webpackDevMiddlewareInstance = webpackDevMiddleware( compiler, {
+  publicPath: config.output.publicPath
+});
+
+app.use(webpackDevMiddlewareInstance);
+
+const server = app.listen(port || 3000);
+console.log('server is listening on port ' + port);
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ type: 'application/json' }));
+app.use(express.static(__dirname));
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * *
+  API Routes
+* * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * *
+  Fallback Routes
+* * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/* Compression to g-zip*/
+app.get('*.js', function (req, res, next) {
+  req.url = req.url + '.gz';
+  res.set('Content-Encoding', 'gzip');
+  next();
+});
+
+app.get('*', (req,res) =>{
+  res.sendFile(path.resolve(__dirname, '../index.html'))
+});
+
+
+
+
+
+module.exports.server = server;
+module.exports.app = app;
+module.exports.webpackDevMiddlewareInstance = webpackDevMiddlewareInstance;
