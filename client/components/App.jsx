68bc0f15930422d08778fb6ee61975eab79502fd
Nick Havens <npahavens@gmail.com>
2017-12-07 21:17:32 -0600

    stub out token and user requests

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import Promise from 'bluebird';
 import { BrowserRouter, Route, Link, browserHistory } from 'react-router-dom';
 import '../css/styles.css';
 
@@ -7,8 +8,16 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-
+      token: ''
     };
+  }
+
+  getToken() {
+
+  }
+
+  getUser() {
+
   }
 
   render() {
