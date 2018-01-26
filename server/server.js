e748a9c71e2f98f01ab7b905774b1d03c2134792
Nick Havens <npahavens@gmail.com>
2018-01-26 16:26:03 -0600

    organize boilerplate server

--- server/server.js
+++ server/server.js
@@ -1,47 +1,42 @@
-
-if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test') {
+/*Initialize Environment*/
+const { NODE_ENV, PORT } = process.env;
+if (NODE_ENV !== 'production' && NODE_ENV !== 'test') {
   require('dotenv').config();
 }
 
+/*Initialize Express*/
 const express = require('express')
 const bodyParser = require('body-parser');
 const path = require('path');
-const webpack = require('webpack');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-
-const { contactRouter } = require('./routes/contactRoutes.js');
-const { caseRouter } = require('./routes/caseRoutes.js');
+const port = PORT || 3000;
 
 const app = express();
-
-const port = process.env.PORT || 3000;
-
-let config;
-(port === 3000)? config = require('../webpack.dev.js') : config = require('../webpack.prod.js');
-const compiler = webpack(config);
-
-app.use(express.static(__dirname));
-
-const webpackDevMiddlewareInstance = webpackDevMiddleware( compiler, {
-  publicPath: config.output.publicPath
-});
-
-app.use(webpackDevMiddlewareInstance);
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({ type: 'application/json' }));
 app.use(express.static(__dirname));
 
+/*Initialize Webpack*/
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const config = require(NODE_ENV === 'production' ? '../webpack.prod.js' : '../webpack.dev.js');
+const compiler = webpack(config);
+const webpackDevMiddlewareInstance = webpackDevMiddleware( compiler, {
+  publicPath: config.output.publicPath
+});
+app.use(webpackDevMiddlewareInstance);
+
 /* * * * * * * * * * * * * * * * * * * * * * * * * * *
   API Routes
 * * * * * * * * * * * * * * * * * * * * * * * * * * */
+const { contactRouter } = require('./routes/contactRoutes.js');
+const { caseRouter } = require('./routes/caseRoutes.js');
+
 app.use('/contact', contactRouter);
-app.use('/cases', caseRouter)
+app.use('/cases', caseRouter);
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * *
   Fallback Routes
 * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
 /* Compression to g-zip*/
 app.get('*.js', function (req, res, next) {
   req.url = req.url + '.gz';
@@ -53,9 +48,9 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../index.html'))
 });
 
-const server = app.listen(port || 3000);
+
+const server = app.listen(port);
 console.log('server is listening on port ' + port);
 
 module.exports.server = server;
-module.exports.app = app;
 module.exports.webpackDevMiddlewareInstance = webpackDevMiddlewareInstance;
