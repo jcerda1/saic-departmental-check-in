695303c4e217839120fde164e53f733e09f85de1
Nick Havens <npahavens@gmail.com>
2018-02-08 16:59:29 -0600

    getContact action sends request to contact endpoint

--- client/src/actions/contactActions.js
+++ client/src/actions/contactActions.js
@@ -2,11 +2,25 @@ import axios from 'axios';
 
 /*** Action Creators ***/
 const receiveContact = (data) => {
-  return {type: 'RECEIVE_CONTACT', contact: data.contact};
+  return {type: 'RECEIVE_CONTACT', contact: data.data.records[0]};
 }
 
 /*** Contact Actions ***/
 
-const getContact = () => (dispatch, getState) => {
+const getContact = (event) => (dispatch, getState) => {
+  const idNum = event.target.value;
 
+  if (idNum.length === 7) {
+    axios.get('/contact', {
+      params: {id: idNum}
+    })
+    .then(data => {
+      dispatch(receiveContact(data));
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
 }
+
+export {getContact};
