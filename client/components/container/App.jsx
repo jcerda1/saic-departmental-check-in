d0dead20c59502cbdf8c3793c4328a1fee9f9ac5
Nick Havens <npahavens@gmail.com>
2018-02-09 16:37:01 -0600

    add Root container

--- client/components/container/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-
-const mapStateToProps = ({ contact }) => {
-  return {
-    contact: contact
-  }
-};
