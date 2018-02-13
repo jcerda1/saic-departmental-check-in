9d50f65db1deb2536995447606e43e80b865a2e2
Nick Havens <npahavens@gmail.com>
2018-02-13 12:21:47 -0600

    add initial empty contact state

--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -5,6 +5,7 @@ import thunk from 'redux-thunk';
 const configureStore = () => {
   return createStore(
     rootReducer,
+    initialState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
     applyMiddleware(thunk)
   );
