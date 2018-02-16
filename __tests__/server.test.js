fe6bda87acc7d1da9d3b3f7ede064a39432a7f07
Nick Havens <npahavens@gmail.com>
2018-02-16 15:20:30 -0600

    add babel for jest testing

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,2 +1,18 @@
 const request = require('supertest');
-const appServer = require('../server/server');
+//const appServer = require('../server/server.js');
+
+// beforeEach(function() {
+//   originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+//   jasmine.DEFAULT_TIMEOUT_INTERVAL = 50000;
+// });
+
+// afterEach(function() {
+//   jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+// });
+
+// afterAll(() => {
+//   console.log('close server');
+//   appServer.server.close();
+//   console.log('close webpack instance');
+//   appServer.webpackDevMiddlewareInstance.close();
+// });
