6447d17e388427249d2f465bbc0b04a1904f70dd
Nick Havens <npahavens@gmail.com>
2018-02-22 20:38:02 -0600

    add mock data

--- test/mocks/contactMock.js
+++ test/mocks/contactMock.js
@@ -1,3 +1,18 @@
 exports.findById = (req, res) => {
-  res.send("TEST");
+  res.json(`{
+    "totalSize": 1,
+    "done": true,
+    "records": [
+        {
+            "attributes": {
+                "type": "Contact",
+                "url": "/services/data/v20.0/sobjects/Contact/003Q000001ASf1aIAD"
+            },
+            "Name": "Nicholas Havens",
+            "Email": "nhaven@saic.edu",
+            "EMPLIDPeoplesoftKey__c": "7000428",
+            "Id": "003Q000001ASf1aIAD"
+        }
+      ]
+    }`);
 };
