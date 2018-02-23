685212065a7a516b3feb927d9a00addd0f0df1fb
Nick Havens <npahavens@gmail.com>
2018-02-23 13:45:06 -0600

    add auth test for cases route

--- test/routeSpec.js
+++ test/routeSpec.js
@@ -79,18 +79,46 @@ describe('Contact Routes', (done) => {
         "totalSize": 1,
         "done": true,
         "records": [
-            {
-                "attributes": {
-                    "type": "Contact",
-                    "url": "/services/data/v20.0/sobjects/Contact/003Q000001ASf1aIAD"
-                },
-                "Name": "Nicholas Havens",
-                "Email": "nhaven@saic.edu",
-                "EMPLIDPeoplesoftKey__c": "7000428",
-                "Id": "003Q000001ASf1aIAD"
-            }
+          {
+            "attributes": {
+                "type": "Contact",
+                "url": "/services/data/v20.0/sobjects/Contact/003Q000001ASf1aIAD"
+            },
+            "Name": "Nicholas Havens",
+            "Email": "nhaven@saic.edu",
+            "EMPLIDPeoplesoftKey__c": "7000428",
+            "Id": "003Q000001ASf1aIAD"
+          }
         ]
       });
+      done();
+    });
+  });
+});
+
+describe('Case Routes', (done) => {
+  let request;
+  let getTokenSpy;
+  let getCasesStub;
+  let caseRoute;
+
+  beforeEach(() => {
+    app = express();
+    getTokenSpy = sinon.spy(getTokenMock);
+
+    caseRoute = proxyquire('../server/routes/caseRoutes.js', {
+      '../controllers/salesforce/auth.js': {
+        getToken: getTokenSpy,
+      }
+    });
+
+    caseRoute(app);
+    request = supertest(app);
+  });
+
+  it('Should pass the request through authentication middleware', (done) => {
+    request.get('/cases').expect(200, (err, res) => {
+      expect(getTokenSpy.called).to.equal(true);
       done();
     });
   });
