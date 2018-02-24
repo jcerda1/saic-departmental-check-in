a26566a4e4b3734901fa2407eafc85a88525583c
Nick Havens <npahavens@gmail.com>
2018-02-24 14:01:10 -0600

    add authSpec for testing auth middleware

---
+++ test/server_test/authSpec.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const sinon = require('sinon');
+const supertest = require('supertest');
+const chai = require('chai');
+const sinonChai = require('sinon-chai');
+const expect = chai.expect;
+const moxios = require('moxios');
+
+chai.use(sinonChai);
+
+describe('Saleforce Auth Controller Tests', () => {
+  it('Should take request and response', (done) => {
+    done();
+  })
+});
