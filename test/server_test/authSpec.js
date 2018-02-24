9251e9878f0b43e4f7b43f837f1902818af0c44a
Nick Havens <npahavens@gmail.com>
2018-02-24 14:25:57 -0600

    remove moxios

--- test/server_test/authSpec.js
+++ test/server_test/authSpec.js
@@ -1,15 +1,42 @@
 const express = require('express');
+const axios = require('axios');
 const sinon = require('sinon');
 const supertest = require('supertest');
 const chai = require('chai');
 const sinonChai = require('sinon-chai');
 const expect = chai.expect;
-const moxios = require('moxios');
+const { getToken } = require('../../server/controllers/salesforce/auth.js');
 
 chai.use(sinonChai);
 
 describe('Saleforce Auth Controller Tests', () => {
+
+  beforeEach(() => {
+
+  });
+
+  afterEach(function () {
+
+  })
+
   it('Should take request and response', (done) => {
-    done();
+    axios.post(`https://test.salesforce.com/services/oauth2/token`, {}, {
+      params: { //Salesforce sandbox creds
+        grant_type: 'password',
+        client_id: 'TEST',
+        client_secret: 'TEST',
+        username: 'TEST',
+        password: 'TEST'
+      }
+    })
+  .then((res) => {
+     done();
+  })
+  .catch(err => {
+    console.log(err);
+     done();
+  });
+
+
   })
 });
