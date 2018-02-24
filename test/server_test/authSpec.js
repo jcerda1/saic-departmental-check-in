e1c2f2593a1348afcfb2298b6ff2ddd526f7a56e
Nick Havens <npahavens@gmail.com>
2018-02-24 14:34:59 -0600

    import proxyquire

--- test/server_test/authSpec.js
+++ test/server_test/authSpec.js
@@ -4,39 +4,23 @@ const sinon = require('sinon');
 const supertest = require('supertest');
 const chai = require('chai');
 const sinonChai = require('sinon-chai');
+const proxyquire = require('proxyquire');
 const expect = chai.expect;
-const { getToken } = require('../../server/controllers/salesforce/auth.js');
 
 chai.use(sinonChai);
 
-describe('Saleforce Auth Controller Tests', () => {
+describe('Saleforce Auth Controller Tests', (url, body, params) => {
+  let axiosPostStub;
 
   beforeEach(() => {
-
+    axiosPostSpy = sinon.spy();
   });
 
   afterEach(function () {
 
   })
 
-  it('Should take request and response', (done) => {
-    axios.post(`https://test.salesforce.com/services/oauth2/token`, {}, {
-      params: { //Salesforce sandbox creds
-        grant_type: 'password',
-        client_id: 'TEST',
-        client_secret: 'TEST',
-        username: 'TEST',
-        password: 'TEST'
-      }
-    })
-  .then((res) => {
-     done();
-  })
-  .catch(err => {
-    console.log(err);
-     done();
-  });
-
+  it('Should make an POST request with axios to the Salesforce auth server', (done) => {
 
   })
 });
