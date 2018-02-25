c32a65cf8d860183b7f42c1ce8db6d37f2c2966e
Nick Havens <npahavens@gmail.com>
2018-02-25 16:23:58 -0600

    add test for error on request without ID param

--- test/server_test/contactSpec.js
+++ test/server_test/contactSpec.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const axios = require('axios');
 const sinon = require('sinon');
-const supertest = require('supertest');
 const chai = require('chai');
 const sinonChai = require('sinon-chai');
 const proxyquire = require('proxyquire');
 const Promise = require('bluebird');
 const expect = chai.expect;
+const supertest = require('supertest');
 const testController = require('../test_helpers/testController.js');
 
 chai.use(sinonChai);
@@ -17,27 +17,30 @@ describe('Contact Controller Tests', () => {
     let axiosGETStub;
     let axiosGETSpy;
     let findById;
+    let request;
+    let app;
 
-    axiosGETStub = (url, body, params) => {
-            return new Promise((resolve, reject) => {
-              resolve({
-                  "totalSize": 1,
-                  "done": true,
-                  "records": [
-                      {
-                          "attributes": {
-                              "type": "Contact",
-                              "url": "/services/data/v20.0/sobjects/Contact/003Q000001ASf1aIAD"
-                          },
-                          "Name": "Nicholas Havens",
-                          "Email": "nhaven@saic.edu",
-                          "EMPLIDPeoplesoftKey__c": "7000428",
-                          "Id": "003Q000001ASf1aIAD"
-                      }
-                  ]
-              });
-            });
-          }
+    beforeEach(() => {
+      axiosGETStub = (url, body, params) => {
+        return new Promise((resolve, reject) => {
+          resolve({
+              "totalSize": 1,
+              "done": true,
+              "records": [
+                  {
+                      "attributes": {
+                          "type": "Contact",
+                          "url": "/services/data/v20.0/sobjects/Contact/003Q000001ASf1aIAD"
+                      },
+                      "Name": "Nicholas Havens",
+                      "Email": "nhaven@saic.edu",
+                      "EMPLIDPeoplesoftKey__c": "7000428",
+                      "Id": "003Q000001ASf1aIAD"
+                  }
+              ]
+          });
+        });
+      }
 
       axiosGETSpy = sinon.spy(axiosGETStub);
 
@@ -47,12 +50,30 @@ describe('Contact Controller Tests', () => {
         }
       }).findById;
 
-      it('makes an axios GET request', (done) => {
-        done();
-      });
+      app = express();
+      request = supertest(app);
+
+      app.get('/test', findById);
+    });
+
   })
 
   describe('findByID Failure Tests', () => {
+    let findById;
+    let request;
+    let app
 
+    beforeEach(() => {
+      findById = require('../../server/controllers/salesforce/contact.js').findById;
+
+      app = express();
+      request = supertest(app);
+
+      app.get('/test', findById);
+    });
+
+    it('Should respond with an error if no ID parameter is present on request', (done) => {
+        request.get('/test').expect(401, done);
+      });
   })
 });
