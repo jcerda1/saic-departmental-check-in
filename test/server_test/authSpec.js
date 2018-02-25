bbe230f3af9c25452b9c5d7115f2a371f53aa721
Nick Havens <npahavens@gmail.com>
2018-02-24 18:25:58 -0600

    use testMiddlware for accessing request and response

--- test/server_test/authSpec.js
+++ test/server_test/authSpec.js
@@ -17,26 +17,40 @@ describe('Saleforce Auth Controller Tests', (url, body, params) => {
   let nextSpy;
   let mockRequest;
   let mockResponse;
-  let checkMiddlware = (middleWare) => {
-
-  }
+  let testMiddleWare;
 
   beforeEach(() => {
     mockRequest = {};
     mockResponse = {send: sinon.spy()};
+
+
+    testMiddleWare = (middleware, done) => {
+      return new Promise((resolve, reject) => {
+        const app = express();
+        const request = supertest(app);
+
+        app.get('/test', middleware, (req, res) => {
+          resolve(req, res);
+        });
+
+        request.get('/test').expect(200, (err, res) => {
+          if (err) {
+            reject(err);
+          }
+          done();
+        });
+      });
+    };
+
     axiosPostStub = (url, body, params) => {
           return new Promise((resolve, reject) => {
-            resolve({data: {access_token: 'TEST'}});
+            resolve({data: {access_token: '00DQ000000GKkqu!AQsAQAxMs'}});
           });
         }
 
     axiosPostSpy = sinon.spy(axiosPostStub);
 
-    nextStub = () => {
-      console.log('TEST')
-    }
-
-    nextSpy = sinon.spy(nextStub);
+    nextSpy = sinon.spy();
 
     getToken = proxyquire('../../server/controllers/salesforce/auth.js', {
       axios: {
@@ -51,25 +65,25 @@ describe('Saleforce Auth Controller Tests', (url, body, params) => {
     done();
   });
 
-   it('Should contain all Salesforce auth params', (done) => {
+  it('Should contain all Salesforce auth params', (done) => {
     getToken(mockRequest, mockResponse, nextSpy);
     const params = axiosPostSpy.args[0][2].params;
     expect(params).to.have.keys('grant_type', 'client_id', 'client_secret', 'username', 'password');
     done()
   });
 
-   it(`Should have a grant_type of 'password'`, (done) => {
+  it(`Should have a grant_type of 'password'`, (done) => {
     getToken(mockRequest, mockResponse, nextSpy);
     const params = axiosPostSpy.args[0][2].params;
     expect(params.grant_type).to.equal('password');
     done()
   });
 
-   it(`Should call next after succesful token request`, (done) => {
+  it(`Should send error data`, (done) => {
     getToken(mockRequest, mockResponse, nextSpy);
-    process.nextTick(() => {
-      expect(nextSpy).to.have.been.called;
-      done();
-    })
+    testMiddleWare(getToken, done)
+    //expect(mockResponse.send).to.have.been.called;
   });
+
+
 });
