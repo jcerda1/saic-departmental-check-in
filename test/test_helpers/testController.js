19d23ffa83f00401f919226109e3a258089ff71b
Nick Havens <npahavens@gmail.com>
2018-02-25 12:57:52 -0600

    remove logging

--- test/test_helpers/testController.js
+++ test/test_helpers/testController.js
@@ -3,17 +3,13 @@ const app = express();
 const supertest = require('supertest');
 const request = supertest(app);
 
-module.exports = (controller, done, cb) => {
-   app.get('/test', controller, (req, res) => {
-    res.send('TEST');
-  });
+module.exports = (controller, cb) => {
+  app.get('/test', controller);
 
   request.get('/test').end((err, res) => {
     if (err) {
-      console.log('test error')
       cb(err);
     } else {
-      console.log('SUCCESS', res.body)
       cb(null, res);
     }
   });
