56cd32943c6be41bbbb3c36fa3e3259793aa6217
Nick Havens <npahavens@gmail.com>
2018-02-24 19:07:32 -0600

    add error test

--- test/server_test/authSpec.js
+++ test/server_test/authSpec.js
@@ -10,92 +10,142 @@ const expect = chai.expect;
 
 chai.use(sinonChai);
 
-describe('Saleforce Auth Controller Tests', (url, body, params) => {
-  let axiosPostStub;
-  let getToken;
-  let nextStub;
-  let nextSpy;
-  let mockRequest;
-  let mockResponse;
-  let testMiddleWare;
-  let axiosPostErrStub;
+describe('Saleforce Auth Controller Tests', () => {
 
-  beforeEach(() => {
-    mockRequest = {};
-    mockResponse = {send: sinon.spy()};
+  describe('Success Tests', () => {
+    let axiosPostStub;
+    let getToken;
+    let nextStub;
+    let nextSpy;
+    let mockRequest;
+    let mockResponse;
+    let testMiddleWare;
+    let axiosPostErrStub;
 
+    beforeEach(() => {
+      mockRequest = {};
+      mockResponse = {send: sinon.spy()};
 
-    testMiddleWare = (middleware, cb) => {
-      const app = express();
-      const request = supertest(app);
+      testMiddleWare = (middleware, done, cb) => {
+        const app = express();
+        const request = supertest(app);
 
-      app.get('/test', middleware, (req, res) => {
-        cb(req, res);
-        res.send('TEST');
-      });
+        app.get('/test', middleware, (req, res) => {
+          cb(req, res);
+          res.send('TEST');
+        });
 
-      request.get('/test').expect(200, (err, res) => {
-      });
-    };
+        request.get('/test').expect(200, (err, res) => {
+          done();
+        });
+      };
 
-    axiosPostStub = (url, body, params) => {
-          return new Promise((resolve, reject) => {
-            resolve({data: {access_token: '00DQ000000GKkqu!AQsAQAxMs'}});
-          });
+      axiosPostStub = (url, body, params) => {
+            return new Promise((resolve, reject) => {
+              resolve({data: {access_token: '00DQ000000GKkqu!AQsAQAxMs'}});
+            });
+          }
+
+      axiosPostErrStub = (url, body, params) => {
+            return new Promise((resolve, reject) => {
+              reject({response: {data: 'TEST ERROR'}});
+            });
+          }
+
+      axiosPostSpy = sinon.spy(axiosPostStub);
+
+      nextSpy = sinon.spy();
+
+      getToken = proxyquire('../../server/controllers/salesforce/auth.js', {
+        axios: {
+          post: axiosPostSpy
         }
+      }).getToken;
+    });
 
-    axiosPostErrStub = (url, body, params) => {
-          return new Promise((resolve, reject) => {
-            reject({response: {data: 'TEST ERROR'}});
-          });
+    it('Should make a POST request with axios to the Salesforce auth server', (done) => {
+      getToken(mockRequest, mockResponse, nextSpy);
+      expect(axiosPostSpy).to.have.been.calledWith(`https://test.salesforce.com/services/oauth2/token`);
+      done();
+    });
+
+    it('Should contain all Salesforce auth params', (done) => {
+      getToken(mockRequest, mockResponse, nextSpy);
+      const params = axiosPostSpy.args[0][2].params;
+      expect(params).to.have.keys('grant_type', 'client_id', 'client_secret', 'username', 'password');
+      done()
+    });
+
+    it(`Should have a grant_type of 'password'`, (done) => {
+      getToken(mockRequest, mockResponse, nextSpy);
+      const params = axiosPostSpy.args[0][2].params;
+      expect(params.grant_type).to.equal('password');
+      done()
+    });
+
+    it(`Should attach token to next request`, (done) => {
+      getToken(mockRequest, mockResponse, nextSpy);
+      testMiddleWare(getToken, done, (req, res) => {
+        expect(req.access_token).to.equal('00DQ000000GKkqu!AQsAQAxMs');
+      })
+    });
+  })
+
+  describe('Failure Tests', () => {
+    let axiosPostStub;
+    let getToken;
+    let nextStub;
+    let nextSpy;
+    let mockRequest;
+    let mockResponse;
+    let testMiddleWare;
+    let axiosPostErrStub;
+
+    beforeEach(() => {
+      mockRequest = {};
+      mockResponse = {send: sinon.spy()};
+
+      testMiddleWare = (middleware, done, cb) => {
+        const app = express();
+        const request = supertest(app);
+
+        app.get('/test', middleware, (req, res) => {
+          cb(req, res);
+          res.send('TEST');
+        });
+
+        request.get('/test').end((err, res) => {
+          done()
+        });
+      };
+
+      axiosPostErrStub = (url, body, params) => {
+            return new Promise((resolve, reject) => {
+              reject({response: {data: 'TEST ERROR'}});
+            });
+          }
+
+      axiosPostSpy = sinon.spy(axiosPostErrStub);
+
+      nextSpy = sinon.spy();
+
+      getToken = proxyquire('../../server/controllers/salesforce/auth.js', {
+        axios: {
+          post: axiosPostSpy
         }
+      }).getToken;
+    });
 
-    axiosPostSpy = sinon.spy(axiosPostStub);
 
-    nextSpy = sinon.spy();
+    it(`Should send error data`, (done) => {
+      getToken(mockRequest, mockResponse, nextSpy);
+      testMiddleWare(getToken, done, (req, res) => {
+        //expect(req.access_token).to.equal('00DQ000000GKkqu!AQsAQAxMs');
+        expect(res.send).to.have.been.called({response: {data: 'TEST ERROR'}})
+      })
+    });
+  })
 
-    getToken = proxyquire('../../server/controllers/salesforce/auth.js', {
-      axios: {
-        post: axiosPostSpy
-      }
-    }).getToken;
-  });
-
-  it('Should make a POST request with axios to the Salesforce auth server', (done) => {
-    getToken(mockRequest, mockResponse, nextSpy);
-    expect(axiosPostSpy).to.have.been.calledWith(`https://test.salesforce.com/services/oauth2/token`);
-    done();
-  });
-
-  it('Should contain all Salesforce auth params', (done) => {
-    getToken(mockRequest, mockResponse, nextSpy);
-    const params = axiosPostSpy.args[0][2].params;
-    expect(params).to.have.keys('grant_type', 'client_id', 'client_secret', 'username', 'password');
-    done()
-  });
-
-  it(`Should have a grant_type of 'password'`, (done) => {
-    getToken(mockRequest, mockResponse, nextSpy);
-    const params = axiosPostSpy.args[0][2].params;
-    expect(params.grant_type).to.equal('password');
-    done()
-  });
-
-  it(`Should attach token to next request`, (done) => {
-    getToken(mockRequest, mockResponse, nextSpy);
-    testMiddleWare(getToken, (req, res) => {
-      expect(req.access_token).to.equal('00DQ000000GKkqu!AQsAQAxMs');
-      done();
-    })
-  });
-
-  it(`Should send error data`, (done) => {
-    getToken(mockRequest, mockResponse, nextSpy);
-    testMiddleWare(getToken, (req, res) => {
-      expect(req.access_token).to.equal('00DQ000000GKkqu!AQsAQAxMs');
-      done();
-    })
-  });
 
 
 });
