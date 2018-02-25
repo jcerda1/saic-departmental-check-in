988d9a99ea4b271c1c8a618b5ac13a2451bf7bf1
Nick Havens <npahavens@gmail.com>
2018-02-25 16:15:13 -0600

    only make axios request if ID is provided

--- server/controllers/salesforce/contact.js
+++ server/controllers/salesforce/contact.js
@@ -1,27 +1,26 @@
 const axios = require('axios');
 
 exports.findById = (req, res) => {
-
   if (!req.query.id) {
-    res.send(401, 'No ID number provided');
+    res.status(401).send('No ID number provided');
+  } else {
+    const url = `https://saic--HDBox.cs3.my.salesforce.com/services/data/v20.0/query?q=`;
+
+    //add fields here
+    const fields = ['name', 'email', 'EMPLIDPeoplesoftKey__c', 'ID'].join(',');
+    const id = req.query.id;
+    const query = `SELECT ${fields} from Contact WHERE EMPLIDPeoplesoftKey__c = '${id}'`;
+
+    axios.get(url + query, {
+      headers: {
+        Authorization: 'Bearer ' + req.access_token
+      }
+    })
+    .then(data => {
+      res.send(data.data);
+    })
+    .catch(err => {
+      res.send(err.response.data);
+    })
   }
-
-  const url = `https://saic--HDBox.cs3.my.salesforce.com/services/data/v20.0/query?q=`;
-
-  //add fields here
-  const fields = ['name', 'email', 'EMPLIDPeoplesoftKey__c', 'ID'].join(',');
-  const id = req.query.id;
-  const query = `SELECT ${fields} from Contact WHERE EMPLIDPeoplesoftKey__c = '${id}'`;
-
-  axios.get(url + query, {
-    headers: {
-      Authorization: 'Bearer ' + req.access_token
-    }
-  })
-  .then(data => {
-    res.send(data.data);
-  })
-  .catch(err => {
-    res.send(err.response.data);
-  })
 }
