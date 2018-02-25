69bed52548f61aed3f7f08c4ee6240f18e0ab9a4
Nick Havens <npahavens@gmail.com>
2018-02-25 13:17:41 -0600

    end response after resolve

--- test/test_helpers/testMiddleWare.js
+++ test/test_helpers/testMiddleWare.js
@@ -8,7 +8,7 @@ module.exports = (middleware) => {
   return new Promise((resolve, reject) => {
     app.get('/test', middleware, (req, res) => {
     resolve(req, res);
-    res.send('TEST');
+    res.end();
     });
 
     request.get('/test').end((err, res) => {
