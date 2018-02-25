c001ced976b234b3bc9ad0101bd646172654f738
Nick Havens <npahavens@gmail.com>
2018-02-25 12:34:12 -0600

    use controller as middlware in route

--- test/test_helpers/testController.js
+++ test/test_helpers/testController.js
@@ -3,17 +3,18 @@ const app = express();
 const supertest = require('supertest');
 const request = supertest(app);
 
-module.exports = (done, cb) => {
-   app.get('/test', (req, res) => {
+module.exports = (controller, done, cb) => {
+   app.get('/test', controller, (req, res) => {
     res.send('TEST');
   });
 
   request.get('/test').end((err, res) => {
+    done();
     if (err) {
       cb(err);
     } else {
       cb(res);
     }
-    done();
+
   });
 }
