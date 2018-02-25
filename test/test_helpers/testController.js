90a3d2ad1520daa7b5f15f642042327b5e6b867e
Nick Havens <npahavens@gmail.com>
2018-02-25 16:16:40 -0600

    use .then instead of .end

--- test/test_helpers/testController.js
+++ test/test_helpers/testController.js
@@ -1,19 +1,16 @@
 const express = require('express');
 const app = express();
 const supertest = require('supertest');
-const request = supertest(app);
 const Promise = require('bluebird');
 
-module.exports = (controller) => {
+module.exports = (controller, params = {}) => {
   return new Promise((resolve, reject) => {
   app.get('/test', controller);
 
-  request.get('/test').end((err, res) => {
-    if (err) {
-      reject(err);
-    } else {
+  const request = supertest(app);
+
+  request.get('/test').then((res) => {
       resolve(res);
-    }
-  });
+    });
   })
 }
