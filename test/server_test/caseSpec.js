168bed780bb67e47ac1a3da6e2b0189934426a35
Nick Havens <npahavens@gmail.com>
2018-02-25 19:30:32 -0600

    add testing boilerplate

---
+++ test/server_test/caseSpec.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const axios = require('axios');
+const sinon = require('sinon');
+const chai = require('chai');
+const sinonChai = require('sinon-chai');
+const proxyquire = require('proxyquire');
+const Promise = require('bluebird');
+const expect = chai.expect;
+const supertest = require('supertest');
+const testController = require('../test_helpers/testController.js');
+
+chai.use(sinonChai);
