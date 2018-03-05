153b2cab12d26f43ea62abd58228bc261890ceb8
Nick Havens <npahavens@gmail.com>
2018-03-05 11:58:33 -0600

    restore hot reloading

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,34 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import configureStore from './store/configureStore.js';
 import { Provider } from 'react-redux';
 import { AppContainer } from 'react-hot-loader';
-import App from '../components/App.jsx';
-import initialState from './reducers/initialState.js';
 import Root from '../containers/Root.js';
-
-//const store = configureStore();
-//const history = syncHistoryWithStore(browserHistory, store);
 
 const render = Component => {
   ReactDOM.render(
-    //<AppContainer>
-      //<Provider store={store}>
-        <Component />,
-      //</Provider>,
-    //</AppContainer>,
+    <AppContainer>
+      <Component />
+    </AppContainer>,
     document.getElementById('root'),
   )
 }
 
-// if (module.hot && HOT) {
-//   render(App);
+if (module.hot && HOT) {
+  render(App);
 
-//   module.hot.accept('../components/App.jsx', () => {
-//     console.log('Reloading')
-//     const nextApp = require('../components/App.jsx').default;
-//     render(nextApp);
-//   })
-// } else {
+  module.hot.accept('../components/App.jsx', () => {
+    console.log('Reloading')
+    const nextApp = require('../components/App.jsx').default;
+    render(nextApp);
+  })
+} else {
   render(Root);
-//}
+}
