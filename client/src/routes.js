477cf1ca1e3e3c7880fe7f1f5e9cd880747df241
Nick Havens <npahavens@gmail.com>
2018-03-06 12:02:35 -0600

    add account route

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Route } from 'react-router';
 import App from '../components/App.jsx';
+import Account from '../components/Account.jsx';
 
 export default (
-  <Route path="/" component={App}>
-  </Route>
+  <div>
+    <Route path="/" component={App}></Route>
+    <Route path="/account" component={Account}></Route>
+  </div>
 )
