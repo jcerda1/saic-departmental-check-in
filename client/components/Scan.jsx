b4afc89b909205e12bca7358de491af69c993638
Nick Havens <npahavens@gmail.com>
2018-03-09 15:56:21 -0600

    use functional stateless component to render scan view

---
+++ client/components/Scan.jsx
@@ -0,0 +1,17 @@
+const Scan = (props) => {
+  (<div className="main">
+    <div className="flex-container title">Please scan your ID</div>
+    <div className="flex-container fa fa-id-card"></div>
+    <div className="flex-container text-input">
+      <form>
+        <input
+          name="idNum"
+          type="text"
+          onChange={}
+        />
+      </form>
+    </div>
+  </div>)
+};
+
+export default Scan;
