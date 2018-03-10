ba1ca030ac07b7c921b6cc1be7107851efdba619
Nick Havens <npahavens@gmail.com>
2018-03-10 17:39:32 -0600

    export connected container

--- client/containers/App.js
+++ client/containers/App.js
@@ -2,12 +2,25 @@ import React from 'react';
 import Account from '../components/Account.jsx';
 import Scan from '../components/Scan.jsx';
 import { connect } from 'react-redux';
-import '../css/styles.css';
 
 const App = (props) => {
-  console.log(props)
-
+  console.log(props.userObj)
   return props.userObj ? <Account/> : <Scan/>;
 };
 
-export default App;
+const mapStateToProps = (state) => {
+    return {
+        userObj: state.contact
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+
+    };
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(App);
