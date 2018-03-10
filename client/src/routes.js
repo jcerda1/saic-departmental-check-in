7a7ca2deb81ef4bc0649e4d1827e8d155aa52ad5
Nick Havens <npahavens@gmail.com>
2018-03-10 16:53:43 -0600

    remove account route

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Route } from 'react-router';
 import App from '../components/App.jsx';
-import Account from '../components/Account.jsx';
 
 export default (
+
   <div>
     <Route path="/" component={App}></Route>
-    <Route path="/account" component={Account}></Route>
   </div>
 )
