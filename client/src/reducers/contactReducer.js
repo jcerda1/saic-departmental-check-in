0eb092c19a7245af66cfa0f3c96e96b43d18d03f
Nick Havens <npahavens@gmail.com>
2018-03-10 19:51:50 -0600

    remove old App component

--- client/src/reducers/contactReducer.js
+++ client/src/reducers/contactReducer.js
@@ -1,7 +1,6 @@
 const contact = (state = '', action) => {
   switch(action.type) {
     case 'RECEIVE_CONTACT':
-      console.log('RECEIVE_CONTACT action');
       let newState = action.contact;
       return newState;
     default:
