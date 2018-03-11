fc35d195b9bb403d3b5c3d54fd4eeb707a873dd5
Nick Havens <npahavens@gmail.com>
2018-03-11 11:06:14 -0500

    add Account container

---
+++ client/containers/Account.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+const Account = (props) => {
+  return ;
+};
+
+const mapStateToProps = (state) => {
+  return {...state};
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+
+    };
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Account);
