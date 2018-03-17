25e285b626ef3b617a62bde4e4a02af1c5e19e17
Nick Havens <npahavens@gmail.com>
2018-03-17 15:41:14 -0500

    cleanup syntax

--- client/src/actions/newCaseActions.js
+++ client/src/actions/newCaseActions.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 /*** Action Creators ***/
 const editSubject = (data) => {
-  return {type: 'EDIT_SUBJECT', subject: data};
+  return { type: 'EDIT_SUBJECT', subject: data };
 }
 
-const setLoading = (data) => {
-  return {type: 'SET_LOADING', loading: data};
+const setLoadingState = (data) => {
+  return { type: 'SET_LOADING', loading: data };
 }
 
 /*** newCase Actions ***/
@@ -15,9 +15,9 @@ const handleEdit = (event) => (dispatch, getState) => {
   dispatch(editSubject(event.target.value));
 }
 
-const toggleLoading = (args) => (dispatch, getState) => {
-  const loading = getState().newCase.loading;
-  dispatch(setLoading(!loading));
+const toggleLoading = () => (dispatch, getState) => {
+  const { loading } = getState().newCase;
+  dispatch(setLoadingState(!loading));
 }
 
-export {handleEdit, toggleLoading};
+export { handleEdit, toggleLoading };
