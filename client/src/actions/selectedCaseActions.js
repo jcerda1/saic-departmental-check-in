4dcf36b005fbbf8ce5d90cc55bdaf7c2532ed3d5
Nick Havens <npahavens@gmail.com>
2018-03-17 18:56:02 -0500

    add setEditing action creator

--- client/src/actions/selectedCaseActions.js
+++ client/src/actions/selectedCaseActions.js
@@ -1,1 +1,14 @@
 import axios from 'axios';
+
+/*** Action Creators ***/
+const setEditing = (data) => {
+  return {
+    type: 'SET_EDITING',
+    editing: data
+  };
+
+
+};
+
+
+/*** Contact Actions ***/
