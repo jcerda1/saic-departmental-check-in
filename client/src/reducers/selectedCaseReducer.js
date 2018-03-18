bce90fc88362cad99651041c9f48c20e3c67d9f2
Nick Havens <npahavens@gmail.com>
2018-03-17 19:05:21 -0500

    handle setLoading action

--- client/src/reducers/selectedCaseReducer.js
+++ client/src/reducers/selectedCaseReducer.js
@@ -2,13 +2,14 @@ import initialState from './initialState.js';
 
 const selectedCase = (state = initialState.selectedCase, action) => {
   const { type, status, loading, editing } = action;
+  let newState;
 
   switch(type) {
     case 'SET_EDITING':
       let newState = { ...state, editing };
       return newState;
-    case 'SET_EDITING':
-      let newState = editing;
+    case 'SET_LOADING':
+      let newState = { ...state, loading };
       return newState;
     default:
       return state;
